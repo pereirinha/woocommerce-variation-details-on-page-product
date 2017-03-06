109833a215bac104f6c017c242118c11a6080cfe
Marco Pereirinha <marcopereirinha@MacBook-Pro-de-Marco.local>
2017-03-06 02:19:59 +0000

    Bump plugin version

--- wc-attributes-on-page.php
+++ wc-attributes-on-page.php
@@ -4,7 +4,7 @@
 Plugin Name: WooCommerce Variation Details on Page Product
 Plugin URI: https://github.com/pereirinha/woocommerce-variation-details-on-page-product
 Description: Display physical size and weight of product within product meta details.
-Version: 3.3.1
+Version: 3.3.2
 Author: Marco Pereirinha
 Author URI: http://www.linkedin.com/in/marcopereirinha
 */
@@ -17,7 +17,7 @@ if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', g
 		class MP_WC_Variation_Details_On_Page_Product {
 
 			// Definition of version
-			const VERSION = '3.3.1';
+			const VERSION             = '3.3.2';
 			const VERSION_OPTION_NAME = 'mp_wc_vdopp_version';
 
 			public $plugin_prefix;
